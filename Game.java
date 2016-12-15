c3ff82a93caeab25d968682e60dfe5f8927ec3ea
Vini Sanches <suiciniv.ms@hotmail.com>
2016-12-15 17:51:37 -0200

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,60 @@
+/**
+ * Created by ViniciusSanches on 15/12/16.
+ */
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.*;
+
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import javax.swing.Timer;
+
+public class Game extends JPanel /*implements KeyListener*/{
+
+    private Player player;
+
+    public Game(){
+
+    }
+
+//    public void getInput(KeyEvent e){
+//
+//        if(e.getKeyCode()==KeyEvent.VK_RIGHT)
+//            player.direction = 1;
+//        if(e.getKeyCode()== KeyEvent.VK_LEFT)
+//            player.direction = -1;
+//
+//    }
+
+//    public void update(){
+//        player.update();
+//    }
+
+    public void render(){
+
+    }
+//
+//    @Override
+//    public void keyTyped(KeyEvent e) {
+//
+//    }
+//
+//    @Override
+//    public void keyPressed(KeyEvent e) {
+//
+//    }
+//
+//    @Override
+//    public void keyReleased(KeyEvent e) {
+//
+//    }
+}
